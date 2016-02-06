d18016af6b344be58b821ce36d80f4028e575fa6
Jens Östlund <jostlund@gmail.com>
2016-02-06 16:02:30 +0100

    Initial commit

---
+++ sliding-window.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function slidingWindow(xs, options) {
+  options = options || {};
+
+  const api = {},
+        windowSize = Math.abs(options.windowSize) || 3,
+        getWindow = windowGetter(Array.prototype.slice.call(xs),
+                                 windowSize);
+
+  let currentIndex = Math.abs(options.startIndex) || 0;
+
+  api.current = () => getWindow(currentIndex);
+
+  api.left = () => {
+    currentIndex = (currentIndex || xs.length) - 1;
+    return getWindow(currentIndex);
+  };
+
+  api.right = () => {
+    currentIndex = (currentIndex + 1) % xs.length;
+    return getWindow(currentIndex);
+  };
+
+  api.reset = () => {
+    currentIndex = Math.abs(options.startIndex) || 0;
+  };
+
+  return api;
+}
+
+function windowGetter(xs, windowSize) {
+  return (startIdx) => {
+    if (xs.length === 0) return [];
+
+    const window = [];
+    for (let i = startIdx; i < (startIdx + windowSize); i++) {
+      window.push(xs[i % xs.length]);
+    }
+    return window;
+  };
+}
+
+module.exports = slidingWindow;
---
+++ test/sliding-window-test.js
@@ -0,0 +1,174 @@
+"use strict";
+
+require("chai").should();
+
+const slidingWindow = require("../sliding-window.js");
+
+describe("Sliding Window", () => {
+
+  it("should have a default window size of 3 with 0 as the default start index", () => {
+    const window = slidingWindow([1, 2, 3, 4, 5]);
+    window.current().should.be.an("array").with.length(3);
+  });
+
+  it("should start the window at the start index if provided", () => {
+    const window = slidingWindow([1, 2, 3, 4], { startIndex: 1 });
+    window.current().should.eql([2, 3, 4]);
+  });
+
+  it("should have a window of the provided size", () => {
+    const window = slidingWindow([1, 2, 3, 4], { windowSize: 2 });
+    window.current().should.eql([1, 2]);
+  });
+
+  it("should return the first window upon a left followed by a right", () => {
+    const window = slidingWindow([1, 2, 3, 4]);
+    const firstWindow = [1, 2, 3];
+
+    window.current().should.eql(firstWindow);
+    window.left().should.eql([4, 1, 2]);
+    window.right().should.eql(firstWindow);
+  });
+
+  it("should return the first window upon a right followed by a left", () => {
+    const window = slidingWindow([1, 2, 3, 4]);
+    const firstWindow = [1, 2, 3];
+
+    window.current().should.eql(firstWindow);
+    window.right().should.eql([2, 3, 4]);
+    window.left().should.eql(firstWindow);
+  });
+
+  describe("current", () => {
+
+    it("should return an empty list if initialized with an empty list", () => {
+      const window = slidingWindow([]);
+      window.current().should.be.an("array").with.length(0);
+    });
+
+    it("should return an array of windowSize size", () => {
+      let window;
+
+      window = slidingWindow([1, 2, 3], { windowSize: 1 });
+      window.current().should.be.an("array").with.length(1);
+
+      window = slidingWindow([1, 2, 3], { windowSize: 2 });
+      window.current().should.be.an("array").with.length(2);
+    });
+
+    it("should repeatedly return the same window", () => {
+      const window = slidingWindow([1, 2, 3], { windowSize: 2 });
+
+      const first = window.current();
+      window.current().should.eql(first);
+      window.current().should.eql(first);
+      window.current().should.eql(first);
+    });
+
+    it("should wrap around if window size is greater than provided list", () => {
+      const window = slidingWindow([1, 2]);
+      window.current().should.eql([1, 2, 1]);
+    });
+
+    it("should wrap around multiple times if necessary", () => {
+      const window = slidingWindow([1, 2, 3], { windowSize: 9 });
+      window.current().should.eql([1, 2, 3, 1, 2, 3, 1, 2, 3]);
+    });
+
+  });
+
+  describe("right", () => {
+
+    it("should return an empty list if initialized with an empty list", () => {
+      const window = slidingWindow([]);
+      window.right().should.be.an("array").with.length(0);
+    });
+
+    it("should return an array of windowSize size", () => {
+      let window;
+
+      window = slidingWindow([1, 2, 3], { windowSize: 1 });
+      window.right().should.be.an("array").with.length(1);
+
+      window = slidingWindow([1, 2, 3], { windowSize: 2 });
+      window.right().should.be.an("array").with.length(2);
+    });
+
+    it("should move the window to the right by one", () => {
+      const window = slidingWindow([1, 2, 3, 4]);
+      window.current().should.eql([1, 2, 3]);
+      window.right().should.eql([2, 3, 4]);
+    });
+
+    it("should wrap around to the beginning of the list if window reaches the end", () => {
+      const window = slidingWindow([1, 2, 3, 4]);
+      window.right().should.eql([2, 3, 4]);
+      window.right().should.eql([3, 4, 1]);
+    });
+
+    it("should wrap multiple times if necessary", () => {
+      let window = slidingWindow([1, 2, 3], { windowSize: 9 });
+      window.right().should.eql([2, 3, 1, 2, 3, 1, 2, 3, 1]);
+      window.right().should.eql([3, 1, 2, 3, 1, 2, 3, 1, 2]);
+    });
+  });
+
+  describe("left", () => {
+
+    it("should return an empty list if initialized with an empty list", () => {
+      const window = slidingWindow([]);
+      window.left().should.be.an("array").with.length(0);
+    });
+
+    it("should return an array of windowSize size", () => {
+      let window;
+
+      window = slidingWindow([1, 2, 3], { windowSize: 1 });
+      window.left().should.be.an("array").with.length(1);
+
+      window = slidingWindow([1, 2, 3], { windowSize: 2 });
+      window.left().should.be.an("array").with.length(2);
+    });
+
+    it("should move the window to the left by one", () => {
+      const window = slidingWindow([1, 2, 3, 4]);
+      window.current().should.eql([1, 2, 3]);
+      window.left().should.eql([4, 1, 2]);
+    });
+
+    it("should wrap multiple times if necessary", () => {
+      let window = slidingWindow([1, 2, 3], { windowSize: 9 });
+      window.left().should.eql([3, 1, 2, 3, 1, 2, 3, 1, 2]);
+      window.left().should.eql([2, 3, 1, 2, 3, 1, 2, 3, 1]);
+    });
+  });
+
+  describe("reset", () => {
+
+    it("should set the window back to its original position", () => {
+      const window = slidingWindow([1, 2, 3, 4]);
+      const originalWindow = window.current();
+
+      window.right().should.not.eql(originalWindow);
+      window.right().should.not.eql(originalWindow);
+
+      window.reset();
+      window.current().should.eql(originalWindow);
+    });
+
+    it("should reset to the provided start index", () => {
+      const window = slidingWindow([1, 2, 3, 4], { startIndex: 2 });
+      const originalWindow = window.current();
+
+      originalWindow.should.eql([3, 4, 1]);
+
+      window.right().should.not.eql(originalWindow);
+      window.right().should.not.eql(originalWindow);
+
+      window.reset();
+      window.current().should.eql(originalWindow);
+    });
+
+  });
+
+});
