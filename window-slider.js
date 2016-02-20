73fd7f0142c75d7e0a51357eec3f60c4f120a7be
Jens Östlund <jostlund@gmail.com>
2016-02-20 16:29:39 +0100

    Added browser compatibility
    * Node module if run in node
    * Exported as `WindowSlider` if run in the browser

--- window-slider.js
+++ window-slider.js
@@ -1,46 +1,58 @@
 "use strict";
 
-function slidingWindow(xs, options) {
-  options = options || {};
+(function () {
 
-  const api = {},
+  function windowGetter(xs, windowSize) {
+    return (startIdx) => {
+      if (xs.length === 0) return [];
+
+      var window = [];
+      for (let i = startIdx; i < (startIdx + windowSize); i++) {
+        window.push(xs[i % xs.length]);
+      }
+      return window;
+    };
+  }
+
+  var windowSlider = function windowSlider(xs, options) {
+    options = options || {};
+
+    var api = {},
         windowSize = Math.abs(options.windowSize) || 3,
         getWindow = windowGetter([].slice.call(xs),
                                  windowSize);
 
-  let currentIndex = Math.abs(options.startIndex) || 0;
+    var currentIndex = Math.abs(options.startIndex) || 0;
 
-  api.current = () => getWindow(currentIndex);
+    api.current = function () {
+      return getWindow(currentIndex);
+    };
 
-  api.left = () => {
-    currentIndex = (currentIndex || xs.length) - 1;
-    return getWindow(currentIndex);
+    api.left = function () {
+      currentIndex = (currentIndex || xs.length) - 1;
+      return getWindow(currentIndex);
+    };
+
+    api.right = function () {
+      currentIndex = (currentIndex + 1) % xs.length;
+      return getWindow(currentIndex);
+    };
+
+    api.reset = function () {
+      currentIndex = Math.abs(options.startIndex) || 0;
+    };
+
+    api.array = function () {
+      return [].slice.call(xs);
+    };
+
+    return api;
   };
 
-  api.right = () => {
-    currentIndex = (currentIndex + 1) % xs.length;
-    return getWindow(currentIndex);
-  };
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = windowSlider;
+  } else {
+    window.WindowSlider = windowSlider;
+  }
 
-  api.reset = () => {
-    currentIndex = Math.abs(options.startIndex) || 0;
-  };
-
-  api.array = () => [].slice.call(xs);
-
-  return api;
-}
-
-function windowGetter(xs, windowSize) {
-  return (startIdx) => {
-    if (xs.length === 0) return [];
-
-    const window = [];
-    for (let i = startIdx; i < (startIdx + windowSize); i++) {
-      window.push(xs[i % xs.length]);
-    }
-    return window;
-  };
-}
-
-module.exports = slidingWindow;
+})();
